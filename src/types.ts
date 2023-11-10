11c718949cdda34385622eb2f414bd5c6fb26799
aashisinghh <aashi30singh@gmail.com>
2023-11-09 17:18:59 -0800

    create tests

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
+export interface TransactionMetaData {
+  initiatedByAddress: string;
+  initiatedAt: Date;
+}
+
 export interface TransferDetails {
-    from: string;
-    to: string;
-    value: number;
-    blockNumber: number;
-    transactionHash: string;
-    gasUsed: bigint;
-    gasPrice: bigint;
-  }
+  from: string;
+  to: string;
+  value: number;
+  blockNumber: number;
+  transactionHash: string;
+  gasUsed: bigint;
+  gasPrice: bigint;
+  transactionMetaData?: TransactionMetaData
+}
